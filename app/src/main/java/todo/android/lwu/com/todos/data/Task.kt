d71443c29e56044612236f088684620eded2564e
Liang Wu <liang_wu@intuit.com>
2017-04-23 15:24:56 -0700

    Add data model: Task

---
+++ app/src/main/java/todo/android/lwu/com/todos/data/Task.kt
@@ -0,0 +1,29 @@
+package todo.android.lwu.com.todos.data
+
+import java.util.*
+
+/**
+ * Created by lwu on 4/23/17.
+ */
+data class Task(
+        val id: String,
+        val title: String,
+        val description: String = UUID.randomUUID().toString(),
+        val completed: Boolean = false
+) {
+    fun getTitleForList(): String {
+        if (!title.isNullOrEmpty()) {
+            return title
+        } else {
+            return description
+        }
+    }
+
+    fun isActive(): Boolean {
+        return !completed
+    }
+
+    fun isEmpty(): Boolean {
+        return title.isNullOrEmpty() && description.isNullOrEmpty()
+    }
+}
